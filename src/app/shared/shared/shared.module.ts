a3b7394718d9906fca55d17f2696b7ec8007d6d3
santosh mahagave <s.mahagave999@gmail.com>
2023-05-11 13:46:26 +0530

    new commit

---
+++ src/app/shared/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  exports:[
+
+  ]
+})
+export class SharedModule { }
